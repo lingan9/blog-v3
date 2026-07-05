5385474751b4724911d89a7d2b71d231a4f96766
oneloveyushi <1905003027@qq.com>
2026-07-05 14:42:35 +0800

    Update blog.config.ts

--- blog.config.ts
+++ blog.config.ts
@@ -1,14 +1,14 @@
 import type { FeedEntry } from './app/types/feed'
 
 const basicConfig = {
-	title: '纸鹿摸鱼处',
-	subtitle: '纸鹿至麓不知路，支炉制露不止漉',
+	title: '凌安的小窝',
+	subtitle: '“在隆冬，我终于知道，我身上有一个不可战胜的夏天。” —— 阿尔贝·加缪',
 	// 长 description 利好于 SEO
-	description: '纸鹿本鹿的个人博客，分享技术与生活。“折腾不止，摸鱼生活——摸门🙏🏻”。纸鹿是一名开源爱好者，结识了许多志同道合的朋友。这个博客记录了他在生活和技术学习中的点滴经历，充满启发与思考。网站界面简洁美观，内容丰富实用，人气互动活跃，涵盖了编程、生活、学习等多个领域，为读者提供了卓越的阅读体验。',
+	description: '凌安的个人博客，目前正在入门ctf-web方向。有无大手子带带我喵',
 	author: {
-		name: '纸鹿本鹿',
+		name: '凌安',
 		avatar: 'https://www.zhilu.site/api/avatar.png',
-		email: 'hi@zhilu.cyou',
+		email: '1905003027@qq.com',
 		homepage: 'https://www.zhilu.site/',
 	},
 	copyright: {
@@ -91,7 +91,7 @@ const blogConfig = {
 /** 用于生成 OPML 和友链页面配置 */
 export const myFeed: FeedEntry = {
 	author: blogConfig.author.name,
-	sitenick: '摸鱼处',
+	sitenick: '凌安的星系',
 	title: blogConfig.title,
 	desc: blogConfig.subtitle || blogConfig.description,
 	link: blogConfig.url,
