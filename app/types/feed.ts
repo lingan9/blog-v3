977545fbd9a0a4bdf629846a90391df3017a2563
oneloveyushi <1905003027@qq.com>
2026-07-06 18:19:11 +0800

    Update feed.ts

--- app/types/feed.ts
+++ app/types/feed.ts
@@ -1,5 +1,7 @@
 import type { FeedGroup } from '../app/types/feed'
+// 友链检测 CLI 需要使用显式导入和相对路径
 import { myFeed } from '../blog.config'
+// eslint-disable-next-line unused-imports/no-unused-imports
 import { getFavicon, getGithubAvatar, getGithubIcon, getOciqGroupAvatar, getOicqAvatar, OicqAvatarSize } from './utils/img'
 
 export default [
@@ -9,20 +11,19 @@ export default [
 		desc: '使用 Clarity 博客主题构建的网站。',
 		// @keep-sorted { "keys": ["date"] }
 		entries: [
-			myFeed,
+			myFeed, // 👈 你的个人博客配置起点
 			
-			// 🎯 100% 对齐严格类型、补齐所有必填字段的 ss0t_HACKED 友链
+			// 🎯 严格契合底层 FeedEntry 类型定义的安全同行友链
 			{
 				author: 'ss0t_HACKED',
 				sitenick: 'ss0t_HACKED',
 				title: 'ss0t_HACKED',
-				desc: '一个超级厉害的pwn大手子',
+				desc: '一个刚刚入门的pwner，大手子ddw',
 				link: 'https://blog.ss0t-hacked.top',
 				feed: 'https://blog.ss0t-hacked.top/atom.xml',
-				icon: 'https://q1.qlogo.cn/g?b=qq&nk=3255154497&s=640',   
-				avatar: 'https://q1.qlogo.cn/g?b=qq&nk=3255154497&s=640', 
-				archs: ['Nuxt', 'Vercel'],
-				date: '2026-07-06',                                       
+				icon: 'https://q1.qlogo.cn/g?b=qq&nk=3255154497&s=640',
+				avatar: 'https://q1.qlogo.cn/g?b=qq&nk=3255154497&s=640',
+				date: '2026-07-06',
 				comment: '安全同行，Pwn 方向大佬',
 			},
 		],
